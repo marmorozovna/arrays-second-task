2f515f4c59097cfd73bc1704968f149393b74fb7
Марина <marina.morozevich@gmail.com>
2023-08-29 14:45:47 +0300

    fixed main

--- src/main/java/ru/morozevich/arrays/_main/_Main.java
+++ src/main/java/ru/morozevich/arrays/_main/_Main.java
@@ -1,49 +1,12 @@
-import java.util.Arrays;
-
 public class _Main {
     public static void main(String[] args) {
-        int[] numbers = {34, 59, 4, 0};
+        int[] numbers = {1, 109, 4, 89};
         for (int i = 0; i < 4; i++) {
-            if (i == 0) {
-                if (numbers[0] > numbers[1]) {
-                    int x = numbers[0];
-                    int y = numbers[1];
-                    numbers[0] = y;
-                    numbers[1] = x;
-                }
-                if (numbers[0] > numbers[2]) {
-                    int x = numbers[0];
-                    int y = numbers[2];
-                    numbers[0] = y;
-                    numbers[2] = x;
-                }
-                if (numbers[0] > numbers[3]) {
-                    int x = numbers[0];
-                    int y = numbers[3];
-                    numbers[0] = y;
-                    numbers[3] = x;
-                }
-            }
-            if (i == 1) {
-                if (numbers[1] > numbers[2]) {
-                    int x = numbers[1];
-                    int y = numbers[2];
-                    numbers[1] = y;
-                    numbers[2] = x;
-                }
-                if (numbers[1] > numbers[3]) {
-                    int x = numbers[1];
-                    int y = numbers[3];
-                    numbers[1] = y;
-                    numbers[3] = x;
-                }
-            }
-            if (i == 2) {
-                if (numbers[2] > numbers[3]) {
-                    int x = numbers[2];
-                    int y = numbers[3];
-                    numbers[2] = y;
-                    numbers[3] = x;
+            for (int a = i + 1; a < 4; a++) {
+                if (numbers[i] > numbers[a]) {
+                    int x = numbers[i];
+                    numbers[i] = numbers[a];
+                    numbers[a] = x;
                 }
             }
         }

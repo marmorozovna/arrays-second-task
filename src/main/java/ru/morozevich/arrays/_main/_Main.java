810fe10c227b319126f68cbb5d54f8d1ac333051
Марина <marina.morozevich@gmail.com>
2023-09-11 15:00:34 +0300

    changed 4s to number.length

--- src/main/java/ru/morozevich/arrays/_main/_Main.java
+++ src/main/java/ru/morozevich/arrays/_main/_Main.java
@@ -1,8 +1,8 @@
 public class _Main {
     public static void main(String[] args) {
         int[] numbers = {1, 109, 4, 89};
-        for (int i = 0; i < 4; i++) {
-            for (int a = i + 1; a < 4; a++) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int a = i + 1; a < numbers.length; a++) {
                 if (numbers[i] > numbers[a]) {
                     int x = numbers[i];
                     numbers[i] = numbers[a];
@@ -10,7 +10,7 @@ public class _Main {
                 }
             }
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
     }
